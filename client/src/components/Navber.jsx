a30994023d5a74645e317f1bc6968918f484761c
Rajan Kumar <raorajan9576@gmail.com>
2023-12-05 21:33:35 +0530

    complete responsive navbar created

--- client/src/components/Navber.jsx
+++ client/src/components/Navber.jsx
@@ -1,14 +1,30 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import { Link } from "react-router-dom";
 import "../styles/Navbar.css";
 import Logo from "../assets/logo-1f6eb176.png";
-import { HiOutlineMenuAlt3 } from "react-icons/hi";
+import { HiOutlineMenuAlt3, HiOutlineX } from "react-icons/hi";
 
 function Navbar() {
-    const [show,setShow] = useState()
+  const [showDrawer, setShowDrawer] = useState(false);
+
+  const toggleDrawer = () => {
+    setShowDrawer(!showDrawer);
+  };
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth > 500) {
+        setShowDrawer(false); 
+      }
+    };
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <div className="Navbar">
-      <div className="Logo">
+      <div className={`Logo ${showDrawer ? "hide-logo" : ""}`}>
         <img src={Logo} alt="logo" width={40} />
       </div>
       <div className="desk-navbar">
@@ -22,7 +38,25 @@ function Navbar() {
           Sign In
         </Link>
       </div>
-      
+
+      <div className="menu-icon" onClick={toggleDrawer}>
+        {showDrawer ? " " : <HiOutlineMenuAlt3 size={35} />}
+      </div>
+      <div className={`drawer ${showDrawer ? "show" : ""}`}>
+        <div className="drawered">
+        <HiOutlineX size={35} onClick={toggleDrawer} />
+        <div className={`Logo ${showDrawer ? "logo" : ""}`}>
+          <img src={Logo} alt="logo" width={40} />
+        </div>
+        <Link to="/">Home</Link>
+        <Link to="/team">Team</Link>
+        <Link to="/contact">Contact</Link>
+        <Link to="/careers">Careers</Link>
+        <Link className="SignIn" to="/signin">
+          Sign In
+        </Link>
+        </div>
+      </div>
     </div>
   );
 }
