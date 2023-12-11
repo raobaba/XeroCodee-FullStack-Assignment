19c8ecc510a572f923d63e891d7aa355917d5728
Rajan Kumar <raorajan9576@gmail.com>
2023-12-12 03:01:32 +0530

    content page created without responsiveness

--- src/app/ContentPage/page.tsx
+++ src/app/ContentPage/page.tsx
@@ -1,23 +1,34 @@
 import React from "react";
 import "../../styles/ContentPage.scss";
+import GoogleCloud from '../../assets/google-cloud.png'
+import Image from "next/image";
 import { FaCircleDot } from "react-icons/fa6";
+import { GoDotFill } from "react-icons/go";
+import { IoMdSync } from "react-icons/io";
+import { FaAws } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaGitlab } from "react-icons/fa6";
+import { IoLogoBitbucket } from "react-icons/io5";
+import { BiLogoPostgresql } from "react-icons/bi";
+import { SiRedis } from "react-icons/si";
+import { SiMongodb } from "react-icons/si";
 
 function ContentPage() {
   return (
     <div className="ContentPage">
       <div className="circle-container">
         <div className="big-circle">
-          <FaCircleDot size={30}/>
+          <FaCircleDot size={30} className="text-green-800" />
         </div>
         <div className="vertical"></div>
 
         <div className="big-circle">
-          <FaCircleDot size={30}/>
+          <FaCircleDot size={30} className="text-green-800" />
         </div>
         <div className="vertical"></div>
 
         <div className="big-circle">
-          <FaCircleDot size={30}/>
+          <FaCircleDot size={30} className="text-green-800" />
         </div>
         <div className="vertical"></div>
       </div>
@@ -28,8 +39,40 @@ function ContentPage() {
             <p>Connect to Cloud</p>
           </div>
           <div className="box-option">
-            <div className="icon-container"></div>
-            <div className="icon-container"></div>
+            <div className="icon-container">
+              <div className="h-16 mt-7 m-auto w-16">
+                <div>
+                  <h1 className="text-xl text-slate-950 font-bold text-center">
+                    AWS
+                  </h1>
+                </div>
+                <div className="flex mt-5 justify-center">
+                  <GoDotFill className="back-red" />
+                  <GoDotFill className="back-green" />
+                  <IoMdSync />
+                </div>
+              </div>
+              <div className="aws-icon h-16 flex justify-center m-auto w-20">
+                <FaAws size={55} />
+              </div>
+            </div>
+            <div className="icon-container">
+              <div className="h-16 mt-7 m-auto w-16">
+                <div>
+                  <h1 className="text-xl text-slate-950 font-bold text-center">
+                    GCP
+                  </h1>
+                </div>
+                <div className="flex mt-5 justify-center">
+                  <GoDotFill className="back-red" />
+                  <GoDotFill className="back-green" />
+                  <IoMdSync />
+                </div>
+              </div>
+              <div className="aws-icon h-16 flex justify-center m-auto w-20">
+                <Image src={GoogleCloud} alt="cloud" className="w-14 h-12 mt-1"/>
+              </div>
+            </div>
           </div>
         </div>
         <div className="step">
@@ -38,9 +81,57 @@ function ContentPage() {
             <p>Connect to Source Code</p>
           </div>
           <div className="box-option">
-            <div className="icon-container"></div>
-            <div className="icon-container"></div>
-            <div className="icon-container"></div>
+            <div className="icon-container">
+              <div className="h-16 mt-7 m-auto w-18">
+                <div>
+                  <h1 className="text-xl text-slate-950 font-bold text-center">
+                    Github
+                  </h1>
+                </div>
+                <div className="flex mt-5 justify-center">
+                  <GoDotFill className="back-red" />
+                  <GoDotFill className="back-green" />
+                  <IoMdSync />
+                </div>
+              </div>
+              <div className="aws-icon h-16 flex justify-center m-auto w-16">
+                <FaGithub size={45} />
+              </div>
+            </div>
+            <div className="icon-container">
+              <div className="h-16 mt-7 m-auto w-16">
+                <div>
+                  <h1 className="text-xl text-slate-950 font-bold text-center">
+                    Gitlab
+                  </h1>
+                </div>
+                <div className="flex mt-5 justify-center">
+                  <GoDotFill className="back-red" />
+                  <GoDotFill className="back-green" />
+                  <IoMdSync />
+                </div>
+              </div>
+              <div className="aws-icon h-16 flex justify-center m-auto w-20">
+                <FaGitlab size={45} />
+              </div>
+            </div>
+            <div className="icon-container">
+              <div className="h-16 mt-7 m-auto w-16">
+                <div>
+                  <h1 className="text-xl text-slate-950 font-bold text-center">
+                    Bitbucket
+                  </h1>
+                </div>
+                <div className="flex mt-5 justify-center">
+                  <GoDotFill className="back-red" />
+                  <GoDotFill className="back-green" />
+                  <IoMdSync />
+                </div>
+              </div>
+              <div className="aws-icon h-16 flex justify-center m-auto w-20">
+                <IoLogoBitbucket size={45} className="text-red-500"/>
+              </div>
+            </div>
           </div>
         </div>
         <div className="step">
@@ -49,9 +140,57 @@ function ContentPage() {
             <p>Connect to Database</p>
           </div>
           <div className="box-option">
-            <div className="icon-container"></div>
-            <div className="icon-container"></div>
-            <div className="icon-container"></div>
+            <div className="icon-container">
+              <div className="h-16 mt-7 m-auto w-16">
+                <div>
+                  <h1 className="text-xl text-slate-950 font-bold text-center">
+                    AWS
+                  </h1>
+                </div>
+                <div className="flex mt-5 justify-center">
+                  <GoDotFill className="back-red" />
+                  <GoDotFill className="back-green" />
+                  <IoMdSync />
+                </div>
+              </div>
+              <div className="aws-icon h-16 flex justify-center m-auto w-20">
+                <SiMongodb size={55} />
+              </div>
+            </div>
+            <div className="icon-container">
+              <div className="h-16 mt-7 m-auto w-16">
+                <div>
+                  <h1 className="text-xl text-slate-950 font-bold text-center">
+                    Redis
+                  </h1>
+                </div>
+                <div className="flex mt-5 justify-center">
+                  <GoDotFill className="back-red" />
+                  <GoDotFill className="back-green" />
+                  <IoMdSync />
+                </div>
+              </div>
+              <div className="aws-icon h-16 flex justify-center m-auto w-20">
+                <SiRedis size={55} />
+              </div>
+            </div>
+            <div className="icon-container">
+              <div className="h-16 mt-7 m-auto w-16">
+                <div>
+                  <h1 className="text-xl text-slate-950 font-bold text-center">
+                    Postgresql
+                  </h1>
+                </div>
+                <div className="flex mt-5 justify-center">
+                  <GoDotFill className="back-red" />
+                  <GoDotFill className="back-green" />
+                  <IoMdSync />
+                </div>
+              </div>
+              <div className="aws-icon h-16 flex justify-center m-auto w-14">
+                <BiLogoPostgresql size={55} className="text-green-800"/>
+              </div>
+            </div>
           </div>
         </div>
       </div>
